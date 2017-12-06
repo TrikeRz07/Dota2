72346c0128837f9c6985e2b13d9062c34d361818
John Patrick Garlit <31005200+TrikeRz07@users.noreply.github.com>
2017-12-06 04:09:06 -0800

    Update process.js

--- www/js/process.js
+++ www/js/process.js
@@ -13,31 +13,6 @@ let process = {
 
     <div className="list-group">
       <ul>
-        <li className="list-group-title"></li>
-        <li><a href='#' onClick={process.NaVi}>
-          <div className="item-content">
-            <div className="item-inner">
-              <div className="item-title">NaVi</div>
-                  <div className="item-after">Details</div>
-            </div>
-          </div></a>
-        </li>
-        <li><a href='#' onClick={process.InvictusGaming}>
-          <div className="item-content">
-            <div className="item-inner">
-              <div className="item-title">InvictusGaming</div>
-                  <div className="item-after">Details</div>
-            </div>
-          </div></a>
-        </li>
-        <li><a href='#' onClick={process.Alliance}>
-          <div className="item-content">
-            <div className="item-inner">
-              <div className="item-title">Alliance</div>
-                  <div className="item-after">Details</div>
-            </div>
-          </div></a>
-        </li>
         <li><a href='#' onClick={process.NewBee}>
           <div className="item-content">
             <div className="item-inner">
@@ -93,71 +68,6 @@ let process = {
             </div>
             <center><img src={"img/imp.jpg"}/>
             <p>asdajslfkjsldkjadlk jalkjlksjdlkaj lkj lakjskldj </p></center>
-            <br/><a href='#' onClick={process.menu}>back</a>
-</div>
-</div>
-</div>
-</div>
-</div>;
-      ReactDOM.render(content,document.getElementById('root'));
-  },
-  NaVi:function(){
-         let content = <div className="page">
-    <div className="page-content infinite-scroll infinite-scroll-top">
-<div className="page-content contacts-content">
-  <div className="list-block contacts-block">
-    <div className="list-group">
-     <div className="navbar">
-              <div className="navbar-inner">
-                <div className="center">NaVi</div>
-              </div>
-            </div>
-            <center><img src={"img/N.jpg"} width={"100%"}/>
-            <p>Natus Vincere also known as NaVi, is a Ukrianian multigaming e-Sports organization. They are the champion of Dota 2 The International 2011.
- </p></center>
-            <br/><a href='#' onClick={process.menu}>back</a>
-</div>
-</div>
-</div>
-</div>
-</div>;
-      ReactDOM.render(content,document.getElementById('root'));
-  },
-  InvictusGaming:function(){
-         let content = <div className="page">
-    <div className="page-content infinite-scroll infinite-scroll-top">
-<div className="page-content contacts-content">
-  <div className="list-block contacts-block">
-    <div className="list-group">
-     <div className="navbar">
-              <div className="navbar-inner">
-                <div className="center">InvictusGaming</div>
-              </div>
-            </div>
-            <center><img src={"img/IG.jpg"} width={"100%"}/>
-            <p>Invictus Gaming is a Chinese multi-platform e-sports organization.They are the champion of Dota 2 The International 2012.
- </p></center>
-            <br/><a href='#' onClick={process.menu}>back</a>
-</div>
-</div>
-</div>
-</div>
-</div>;
-      ReactDOM.render(content,document.getElementById('root'));
-  },
-  Alliance:function(){
-  let content = <div className="page">
-    <div className="page-content infinite-scroll infinite-scroll-top">
-<div className="page-content contacts-content">
-  <div className="list-block contacts-block">
-    <div className="list-group">
-     <div className="navbar">
-              <div className="navbar-inner">
-                <div className="center">Alliance</div>
-              </div>
-            </div>
-            <center><img src={"img/A.jpg"} width={"100%"}/>
-            <p>Alliance is a Swedish professional gaming organization formed in 2013. They are the champion of Dota 2 The International 2013. </p></center>
             <br/><a href='#' onClick={process.menu}>back</a>
 </div>
 </div>
