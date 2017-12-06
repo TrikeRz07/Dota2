8364357848bcd2d57a105ef92de8eb3b11afbd35
John Patrick Garlit <31005200+TrikeRz07@users.noreply.github.com>
2017-12-06 18:07:00 +0800

    Update process.js

--- www/js/process.js
+++ www/js/process.js
@@ -7,25 +7,25 @@ let process = {
     <div className="list-group">
      <div className="navbar">
               <div className="navbar-inner">
-                <div className="center">American Football</div>
+                <div className="center">Dota 2 The International Champions</div>
               </div>
             </div>
 
     <div className="list-group">
       <ul>
         <li className="list-group-title"></li>
-        <li><a href='#' onClick={process.Navi}>
+        <li><a href='#' onClick={process.NaVi}>
           <div className="item-content">
             <div className="item-inner">
-              <div className="item-title">Navi</div>
+              <div className="item-title">NaVi</div>
                   <div className="item-after">Details</div>
             </div>
           </div></a>
         </li>
-        <li><a href='#' onClick={process.IG}>
+        <li><a href='#' onClick={process.InvictusGaming}>
           <div className="item-content">
             <div className="item-inner">
-              <div className="item-title">IG</div>
+              <div className="item-title">InvictusGaming</div>
                   <div className="item-after">Details</div>
             </div>
           </div></a>
@@ -77,7 +77,7 @@ let process = {
 </div>;
       ReactDOM.render(content,document.getElementById('root'));
   },
-  Navi:function(){
+  NaVi:function(){
          let content = <div className="page">
     <div className="page-content infinite-scroll infinite-scroll-top">
 <div className="page-content contacts-content">
@@ -85,11 +85,19 @@ let process = {
     <div className="list-group">
      <div className="navbar">
               <div className="navbar-inner">
-                <div className="center">Navi</div>
+                <div className="center">NaVi</div>
               </div>
             </div>
-            <center><img src={"img/L.jpg"} width={"100%"}/>
-            <p>A linebacker (LB or backer) is a playing position in American football and Canadian football. Linebackers are members of the defensive team, and line up approximately three to five yards (4 m) behind the line of scrimmage, behind the defensive linemen, and therefore "back up the line."
+            <center><img src={"img/N.jpg"} width={"100%"}/>
+            <p>Natus Vincere (Latin: "born to win"), also known as NaVi, is a Ukrianian multigaming e-Sports organization.
+            The International Dota 2 Championships was a double-elimination offline tournament organized by Valve and took place during the five day trade show GamesCom in Cologne, 2011. The venue was used by Valve to unveil Dota 2 to the worldwide audience and offered a staggering $1.6 million prize pool to 16 invited DotA teams from around the world. The game itself was given to the 16 teams some time before the event to prepare for what would be an incredible tournament promising to grab the eSport world's attention.
+NaVi defeated EHOME in the Grand Finals 3-1 with a 1-game advantage, and took home $1,000,000 in prize money.<br>
+Players: <br>
+Artstyle<br>
+Dendi <br>
+XBOCT <br>
+Puppey <br>
+LighTofHeaveN <br>
  </p></center>
             <br/><a href='#' onClick={process.menu}>back</a>
 </div>
@@ -99,7 +107,7 @@ let process = {
 </div>;
       ReactDOM.render(content,document.getElementById('root'));
   },
-  IG:function(){
+  InvitusGaming:function(){
          let content = <div className="page">
     <div className="page-content infinite-scroll infinite-scroll-top">
 <div className="page-content contacts-content">
@@ -107,11 +115,19 @@ let process = {
     <div className="list-group">
      <div className="navbar">
               <div className="navbar-inner">
-                <div className="center">IG</div>
+                <div className="center">InvictusGaming</div>
               </div>
             </div>
-            <center><img src={"img/L1.jpg"} width={"100%"}/>
-            <p>A quarterback (commonly abbreviated "QB") is a position in American and Canadian football. Quarterbacks are members of the offensive team and line up directly behind the offensive line.
+            <center><img src={"img/IG.jpg"} width={"100%"}/>
+            <p>Invictus Gaming (iG) is a Chinese multi-platform e-sports organization.
+            The International Dota 2 Championship was the world's largest Dota 2 tournament, based on prize pot. The event was held from August 26 to September 2, 2012, at Benaroya Hall in Seattle, Washington. This event is the second tournament to bear the name 'The International'. The first International was held in 2011 and functioned as the launch date of the Dota 2 beta.
+Out of the 16 participating teams, 14 were invited, with the remaining two receiving their place in the tournament via two invite-only geographic qualifier tournaments, the "West Qualifier" and the "East Qualifier." Like the first International, a total of $1,600,000 USD was spread among the top 8 teams.
+Players: <br>
+ChuaN<br>
+Faith<br>
+Ferrari_430<br>
+Zhou<br>
+YYF<br>
  </p></center>
             <br/><a href='#' onClick={process.menu}>back</a>
 </div>
@@ -132,8 +148,13 @@ let process = {
                 <div className="center">Alliance</div>
               </div>
             </div>
-            <center><img src={"img/L2.jpg"} width={"100%"}/>
-            <p>Placekicker, or simply kicker (PK or K), is the player in American and Canadian football who is responsible for the kicking duties of field goals and extra points.. </p></center>
+            <center><img src={"img/A.jpg"} width={"100%"}/>
+            <p>Alliance is a Swedish professional gaming organization formed in 2013.
+            The International Dota 2 Championship was held in Seattle, Washington, at the Benaroya Hall. It was the world's largest Dota 2 tournament, based on prize pool. The event was held from August 8 to August 11, 2013, at Benaroya Hall in Seattle, Washington. This event is the third tournament to bear the name 'The International.' The first International was held in 2011 at Gamescom while the second International was held in 2012 in Seattle.
+Out of the 16 participating teams, 13 were invited, with two of the remaining teams receiving their place in the tournament via two invite-only geographic qualifier tournaments, the "West Qualifier" and the "East Qualifier." The final team received their place by winning the Wildcard Match between the runners-up of the two qualifier tournaments.
+The base prize pool was the same as the one used at the first and second Internationals. However, 25% of the revenue generated by the sale of the Interactive Compendium was redirected to the prize pool. The final prize pool was $2,874,380 ($1,274,380 added) USD.
+
+              </p></center>
             <br/><a href='#' onClick={process.menu}>back</a>
 </div>
 </div>
@@ -153,8 +174,15 @@ let process = {
                 <div className="center">NewBee</div>
               </div>
             </div>
-            <center><img src={"img/L3.jpg"} width={"100%"}/>
-            <p>A running back (RB) is an American and Canadian football position, a member of the offensive backfield. ... There are usually one or two running backs on the field for a given play, depending on the offensive formation. </p></center>
+            <center><img src={"img/NB.jpg"} width={"100%"}/>
+            <p>Newbee is a professional Dota 2 team based in Shanghai, China.
+            The International Dota 2 Championships was held in Seattle, Washington. This tournament was the fourth edition of The International, and it was the third consecutive year that the tournament had been hosted in Seattle. Unlike the previous two years, the tournament was held at the KeyArena, a multi-purpose arena in Seattle Center with a total seating capacity of over 17,000.
+A grand total of 16 teams competed in the main event of the tournament. 11 of those received a direct invite, while 4 teams had to qualify through separate regional qualifiers, with the runners up in those qualifiers getting one last chance to compete for the final spot in the main tournament.
+The initial prize pool of the tournament was $1,600,000 USD. 
+An additional $2.50 USD (25%) was added to the total prize pool for every TI4 Compendium sold.
+There is also an option to buy points to level up the compendium in game and 25% of these purchases goes towards the prize pool of the event. 
+The final prize pool is $10,923,977 ($9,323,977 added).
+              </p></center>
             <br/><a href='#' onClick={process.menu}>back</a>
 </div>
 </div>
